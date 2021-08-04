60465505508a02da0ae42bad0b71cbfa6e0dece6
jamesjj1979 <jimjorgensen587@gmail.com>
2021-08-04 07:45:04 -0400

    Create NewComponent.vue

---
+++ custom/components/NewComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :style="containerStyle">
+
+  Test
+  </div>  
+</template>
+
+<script>
+module.exports = {
+  name: 'NewComponent',
+  props: { 
+    items: { type: Array, default: () => ([]) },
+    viewerIsActive: Boolean
+  },
+  data: () => ({
+    viewerLabel: 'New Component',
+    viewerIcon: 'fas fa-file-code',
+    dependencies: []
+  }),
+  computed: {
+    containerStyle() { return { 
+      position: 'relative',
+      height: this.viewerIsActive ? '100%' : '0', 
+      overflowY: 'auto !important' 
+    }},
+  },
+  mounted() { this.loadDependencies(this.dependencies, 0, this.init) },
+  methods: {
+    init() {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
