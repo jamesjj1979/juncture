2e372e903055fe29f23c7e4063fbf54d728fef63
jamesjj1979 <jimjorgensen587@gmail.com>
2021-08-12 11:26:12 -0400

    Update quotes.vue

--- custom/components/quotes.vue
+++ custom/components/quotes.vue
@@ -1,44 +1,35 @@
 <template>
   <div :style="containerStyle">
-
-<div id="jim"></div>
-please 
+<div id="j">working</div>
+  Test
   </div>  
 </template>
 
 <script>
-
-
 module.exports = {
-  name: 'coords',
+  name: 'quotes',
   props: { 
     items: { type: Array, default: () => ([]) },
     viewerIsActive: Boolean
-   
   },
   data: () => ({
     viewerLabel: 'New Component',
     viewerIcon: 'fas fa-file-code',
     dependencies: []
- 
-    
-    
   }),
   computed: {
-    containerStyle() { 
-    return { 
+    containerStyle() { return { 
       position: 'relative',
-      height: this.viewerIsActive ? '100%' : '0',
+      height: this.viewerIsActive ? '100%' : '0', 
       overflowY: 'auto !important' 
     }},
-     
   },
   mounted() { this.loadDependencies(this.dependencies, 0, this.init) },
   methods: {
     init() {
     }
   }
-
+}
 </script>
 
 <style>
